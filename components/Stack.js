563b4990414ae9e92a6de83625effe2cff4ff885
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2023-11-20 17:47:28 +0100

    made stack more responsible

--- components/Stack.js
+++ components/Stack.js
@@ -1,13 +1,25 @@
-// components/Stack.js
-
 import React from 'react';
-import Card from './Card'; // Adjust the path based on your project structure
+import Card from './Card';
 
 const Stack = ({ stackData }) => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: '20px',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+    }}>
       {Object.entries(stackData).map(([suit, card], index) => (
-        <Card key={index} value={suit} style={{ margin: '0 10px' }} />
+        <Card
+          key={index}
+          value={suit}
+          style={{
+            margin: '0 10px',
+            // Add additional styles as needed
+          }}
+        />
       ))}
     </div>
   );
