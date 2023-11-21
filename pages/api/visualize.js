ec8ed4703bf60c117fb6988452378f5f6eb1e657
Patryk Świat <patrykswiat@gmail.com>
2023-11-21 13:57:20 +0100

    fix cors

--- pages/api/visualize.js
+++ pages/api/visualize.js
@@ -1,11 +1,30 @@
+import cors from 'cors';
+
 let data_saved = null;
 
-export default function handler(req, res) {
-  if (req.method === 'POST') {
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your actual frontend origin
+  methods: 'GET,POST',
+  allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  credentials: true,
+  optionsSuccessStatus: 204, // A 204 status code will be sent for successful OPTIONS requests
+};
+
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    // Handle preflight requests
+    cors(corsOptions)(req, res, () => {
+      res.status(200).end();
+    });
+  } else if (req.method === 'POST') {
     data_saved = req.body;
-    res.status(200).json({ data: data_saved });
+    cors(corsOptions)(req, res, () => {
+      res.status(200).json({ data: data_saved });
+    });
   } else if (req.method === 'GET') {
-    res.status(200).json({ data: data_saved });
+    cors(corsOptions)(req, res, () => {
+      res.status(200).json({ data: data_saved });
+    });
   } else {
     // Return a 405 Method Not Allowed for non-POST requests
     res.status(405).end();
