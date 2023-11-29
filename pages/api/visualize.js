c3a104d64b1792639d362b1a480610577ac18aa1
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2023-11-29 17:48:17 +0100

    some backend adjustments, it adds more clarity and it works since first post.

--- pages/api/visualize.js
+++ pages/api/visualize.js
@@ -1,32 +1,40 @@
+// /api/visualize.js
+
+import { createRouter } from 'next-connect';
 import cors from 'cors';
 
 let data_saved = null;
 
 const corsOptions = {
-  origin: 'frontend:3000', // Replace with your actual frontend origin
+  origin: 'http://frontend:3000', // Replace with your actual frontend origin
   methods: 'GET,POST',
   allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
   credentials: true,
-  optionsSuccessStatus: 204, // A 204 status code will be sent for successful OPTIONS requests
+  optionsSuccessStatus: 204,
 };
 
-export default async function handler(req, res) {
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests
-    cors(corsOptions)(req, res, () => {
-      res.status(200).end();
-    });
-  } else if (req.method === 'POST') {
-    data_saved = req.body;
-    cors(corsOptions)(req, res, () => {
-      res.status(200).json({ data: data_saved });
-    });
-  } else if (req.method === 'GET') {
-    cors(corsOptions)(req, res, () => {
-      res.status(200).json({ data: data_saved });
-    });
-  } else {
-    // Return a 405 Method Not Allowed for non-POST requests
-    res.status(405).end();
-  }
-}
+const handler = createRouter();
+
+// Add CORS middleware
+handler.use(cors(corsOptions));
+
+
+// Route handler for POST requests
+handler.post(async (req, res) => {
+  data_saved = req.body;
+  console.log(data_saved)
+  res.status(200).json({ data: data_saved });
+});
+
+// Route handler for GET requests
+handler.get((req, res) => {
+  console.log(data_saved)
+  res.status(200).json({ data: data_saved });
+});
+
+// Return a 405 Method Not Allowed for non-POST requests
+handler.all((req, res) => {
+  res.status(405).end();
+});
+
+export default handler.handler();
